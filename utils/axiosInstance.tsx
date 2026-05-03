0759eaa54a87b9e6354e212c2547f7fd95fc8e2c
Kagiso Tsiane <kagiso258@gmail.com>
2026-05-03 18:00:26 +0200

    feat: align client auth flow with cookie sessions refs #55

--- utils/axiosInstance.tsx
+++ utils/axiosInstance.tsx
@@ -1,14 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = () => {
-  const token =
-    typeof window !== "undefined" ? sessionStorage.getItem("token") : null;
-
   return axios.create({
     baseURL: "/api/backend",
     headers: {
       "Content-Type": "application/json",
-      ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
     withCredentials: true,
   });
